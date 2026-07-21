952965b23b34090c041f847ed6f61488a4670cf5
aditya-3645 <dhumaladitya2005@gmail.com>
2026-07-21 20:11:21 +0530

    running model commit

--- app/llm/gemini_llm.py
+++ app/llm/gemini_llm.py
@@ -5,14 +5,13 @@ from langchain_google_genai import ChatGoogleGenerativeAI
 
 load_dotenv()
 
-
 def get_llm():
     """
     Returns the Gemini LLM.
     """
 
     llm = ChatGoogleGenerativeAI(
-        model="gemini-2.5-flash",
+        model="gemini-flash-latest",
         google_api_key=os.getenv("GOOGLE_API_KEY"),
         temperature=0
     )
